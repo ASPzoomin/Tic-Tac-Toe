1129a794f5a87f6b2ab0470d3d88fd779ba5d33f
Ammula Sai Pavan <ammula.pavan@in.ey.com>
2022-04-23 23:32:43 +0530

    optimised code for tic ta toe

--- src/App.js
+++ src/App.js
@@ -1,86 +1,70 @@
 import { useState } from "react";
 import "./App.css";
 const App = () => {
-  const [turn, setTurn] = useState('X');
-  const [cells,setCells] = useState(Array(9).fill(''));
-  const [winner,setWinner] = useState('');
-  const checkWinner = (squares)=> {
+  const [turn, setTurn] = useState("X");
+  const [cells, setCells] = useState(() =>Array(9).fill(""));
+  const [winner, setWinner] = useState("");
+  const checkWinner = (squares) => {
     const combos = {
-      across:[
-        [0,1,2],
-        [3,4,5],
-        [6,7,8]
+      across: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
       ],
-      bottom:[
-        [0,3,6],
-        [1,4,7],
-        [2,5,8]
+      bottom: [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
       ],
-      diagonal:[
-        [0,4,8],
-        [2,4,6]
-      ]
-    }
-    for(let combo in combos){
-      combos[combo].forEach((pattern)=>{
-        if(
+      diagonal: [
+        [0, 4, 8],
+        [2, 4, 6],
+      ],
+    };
+    for (let combo in combos) {
+      combos[combo].forEach((pattern) => {
+        if (
           squares[pattern[0]] === squares[pattern[1]] &&
-          squares[pattern[1]] === squares[pattern[2]]){
-            setWinner(squares[pattern[0]])
-          }
-      })
+          squares[pattern[1]] === squares[pattern[2]]
+        ) {
+          setWinner(squares[pattern[0]]);
+        }
+      });
     }
-  }
-  const handleClick = (num)=>{
-    if(cells[num] !==''){
-      alert('already clicked');
+  };
+  const handleClick = (num) => {
+    if (cells[num] !== "") {
+      alert("already clicked");
       return;
     }
     const squares = [...cells];
-    if(turn === 'X'){
-      squares[num] = 'X';
-      setTurn('O');
+    if (turn === "X") {
+      squares[num] = "X";
+      setTurn("O");
     } else {
-      squares[num] = 'O';
-      setTurn('X')
+      squares[num] = "O";
+      setTurn("X");
     }
-    checkWinner(squares)
+    checkWinner(squares);
     setCells(squares);
-  }
+  };
 
-  const restartGame = ()=> {
+  const restartGame = () => {
     setWinner(null);
-    setCells(Array(9).fill(''));
-  }
-  const Cell = ({num}) => {
-    return <td onClick={()=>handleClick(num)}>{cells[num]}</td>;
+    setCells(Array(9).fill(""));
   };
   return (
     <div className="container">
-      <table>
       Next Turn:{turn}
-        <tbody>
-          <tr>
-            <Cell  num={0}/>
-            <Cell  num={1}/>
-            <Cell  num={2}/>
-          </tr>
-          <tr>
-            <Cell  num={3}/>
-            <Cell  num={4}/>
-            <Cell  num={5}/>
-          </tr>
-          <tr>
-            <Cell  num={6}/>
-            <Cell  num={7}/>
-            <Cell  num={8}/>
-          </tr>
-        </tbody>
-      </table>
+      <div className="grid-box">
+        {cells.map((x,i)=>(
+            <div key={i} onClick={() => handleClick(i)}>{cells[i]}</div>
+        ))}
+      </div>
       {winner && (
         <>
-        {winner} is the winner
-        <button onClick={()=>restartGame()}>Play Again</button>
+          {winner} is the winner
+          <button onClick={() => restartGame()}>Play Again</button>
         </>
       )}
     </div>
